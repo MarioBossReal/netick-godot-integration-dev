b829275a9c81ed3f4133e0751663083676fbc7e9
MarioBossReal <peternajjar2002@gmail.com>
2024-01-02 16:10:42 +1100

    Removed old code from NetickGodotEditor.cs

--- Netick For Godot 0.8.6 - Development/addons/NetickForGodot/NetickGodotEditor.cs
+++ Netick For Godot 0.8.6 - Development/addons/NetickForGodot/NetickGodotEditor.cs
@@ -8,7 +8,6 @@ using Netick.CodeGen;
 using Netick.GodotEngine;
 using Netick.GodotEngine.Constants;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using static Netick.GodotEngine.NetickInspectorPlugin;
 
@@ -195,14 +194,13 @@ public partial class NetickGodotEditor : EditorPlugin
         }
     }
 
-    // Currently does not setup child prefabs like the original code does. Because this is going to be changed at some point.
-    private ResourceReference RegisterNetworkedPrefab(Node sceneRoot)
+    private void RegisterNetworkedPrefab(Node sceneRoot)
     {
         if (sceneRoot == null)
-            return null;
+            return;
 
         if (sceneRoot.SceneFilePath == null || sceneRoot.SceneFilePath == string.Empty)
-            return null;
+            return;
 
         var path = sceneRoot.SceneFilePath;
 
@@ -210,7 +208,7 @@ public partial class NetickGodotEditor : EditorPlugin
 
         // Already registered.
         if (_netickConfig.Prefabs.ContainsKey(name))
-            return null;
+            return;
 
         var reference = new ResourceReference();
         reference.Name = name;
@@ -221,8 +219,6 @@ public partial class NetickGodotEditor : EditorPlugin
         _dock.AddPrefabReferenceToList(reference);
 
         ResourceSaver.Save(_netickConfig, _netickConfig.ResourcePath);
-
-        return reference;
     }
 
     // Temporary! To allow registering levels in the same way as before.
@@ -273,8 +269,6 @@ public partial class NetickGodotEditor : EditorPlugin
         _dock.RemovePrefabReferenceFromList(reference);
 
         ResourceSaver.Save(_netickConfig, _netickConfig.ResourcePath);
-
-        //GD.Print("Netick: Unregistered prefab: " + Path.GetFileName(sceneRoot.SceneFilePath));
     }
 
     private static NetickGodotEditorConfig GetNetickEditorConfigResource(string path)
@@ -307,223 +301,6 @@ public partial class NetickGodotEditor : EditorPlugin
     {
         var k = ProjectSettings.GlobalizePath(_editorConfig.MainEditorGameAssemblyPath);
         Netick.CodeGen.Processor.ProcessAssembly(new GodotCodeGen(), k);
-    }
-
-    /*---------------------------------------------------------------------------------------------------------*/
-
-    // To be deleted.
-    private void RegisterStuff()
-    {
-        var scenes = GetAllFilesStartingWith(ProjectSettings.GlobalizePath("res://"), ".tscn", null);
-        var levels = new List<string>();
-        var prefabs = new List<string>();
-
-        for (int i = 0; i < scenes.Count; i++)
-        {
-            var scene = GD.Load<PackedScene>(scenes[i]);
-            var instance = scene.Instantiate();
-
-            if (IsNetworkObject(instance))
-                prefabs.Add(scenes[i]);
-            else if (IsNetworkLevel(instance, scenes[i]))
-            {
-                levels.Add(scenes[i]);
-            }
-
-            instance.QueueFree();
-        }
-
-        var config = _netickConfig;
-        config.Set(nameof(NetickConfig.Levels), LocalizeArrayElements(levels.ToArray()));
-        config.Set(nameof(NetickConfig.Prefabs), LocalizeArrayElements(prefabs.ToArray()));
-
-        GD.Print("Netick Editor: registering levels and prefabs done.");
-
-        SaveNetickResource(config);
-        ProcessObjects();
-    }
-
-    // To be deleted.
-    private void ProcessObjects()
-    {
-        ProcessSceneObjects();
-        if (_netickConfig.Get(nameof(NetickConfig.Prefabs)).AsStringArray() != null)
-            ProcessPrefabs(_netickConfig.Get(nameof(NetickConfig.Prefabs)).AsStringArray());
-    }
-
-    // To be deleted.
-    internal static void ProcessSceneObjects()
-    {
-        var editorInterface = EditorInterface.Singleton;
-        var rootChilds = editorInterface.GetEditedSceneRoot().GetChildren();
-        int counter = 0;
-
-        for (int i = 0; i < rootChilds.Count; i++)
-            ProcessSceneObject(rootChilds[i], ref counter);
-
-        GD.Print("Netick Editor: updating scene objects done.");
-    }
-
-    // To be deleted.
-    internal static void ProcessSceneObject(Node parent, ref int sceneIdCounter)
-    {
-        if (IsNetworkObject(parent))
-        {
-            SetPrefabNodeData(parent, -1, null, -1);
-            parent.Set(nameof(NetworkObject.SceneId), sceneIdCounter);
-            SetPrefabNodeChildren(parent, new Node[0]);
-            sceneIdCounter++;
-        }
-
-        foreach (var obj in parent.GetChildren())
-            ProcessSceneObject(obj, ref sceneIdCounter);
-    }
-
-    // To be deleted.
-    void ProcessPrefabs(string[] prefabPaths)
-    {
-        int id = 0;
-
-        foreach (var prefab in prefabPaths)
-            RegisterPrefab(prefab, ref id);
-
-        GD.Print("Netick Editor: updating network prefabs done.");
-
-    }
-
-    // To be deleted.
-    internal void RegisterPrefab(string prefabPath, ref int id)
-    {
-        var scene = GD.Load<PackedScene>(prefabPath);
-        var obj = scene.Instantiate();
-
-        if (!IsNetworkObject(obj))
-        {
-            GD.PrintErr("Netick Editor: registering prefab failed because the prefab does not have a NetworkObject root.");
-            return;
-        }
-
-        if (obj == null)
-        {
-            GD.PrintErr("Netick Editor: registering prefab failed.");
-            return;
-        }
-
-        SetPrefabNodeData(obj, id, null, -1);
-        var childs = new List<Node>();
-
-        foreach (Node child in obj.GetChildren())
-            SetPrefabId(child, id, childs, obj);
-
-        SetPrefabNodeChildren(obj, childs.ToArray());       //   obj.Children = childs.ToArray();
-        id++;
-
-        var packedScene = new PackedScene();
-        packedScene.Pack(obj);
-        ResourceSaver.Save(packedScene, prefabPath);
-        packedScene.TakeOverPath(prefabPath);
-        EditorInterface.Singleton.GetResourceFilesystem().UpdateFile(prefabPath);   //GetEditorInterface().GetResourceFilesystem().Scan();
-    }
-
-    // To be deleted.
-    internal static void SetPrefabId(Node obj, int prefabId, List<Node> rootChildren, Node root)
-    {
-        if (rootChildren != null && IsNetworkObject(obj))
-        {
-            SetPrefabNodeData(obj, prefabId, root, rootChildren.Count);   // GD.Print("Child: " + obj.Name + " Type: " + obj.GetType());
-            SetPrefabNodeChildren(obj, new Node[0]);
-            rootChildren.Add(obj);  // net.Children    = new NetworkObject[0];
-        }
-
-        foreach (Node child in obj.GetChildren())
-            SetPrefabId(child, prefabId, rootChildren, root);
-    }
-
-    // To be deleted.
-    internal static void SetPrefabNodeData(Node node, int prefabId, Node prefabRoot, int prefabIndex)
-    {
-        node.Set(nameof(NetworkObject.SceneId), -1);
-        node.Set(nameof(NetworkObject.PrefabId), prefabId);
-        node.Set(nameof(NetworkObject.InternalPrefabRoot), prefabRoot);
-        node.Set(nameof(NetworkObject.PrefabIndex), prefabIndex);
-    }
-
-    // To be deleted.
-    internal static void SetPrefabNodeChildren(Node node, Node[] children)
-    {
-        var godotArray = new Godot.Collections.Array(children);
-        //node.Set(nameof(NetworkObject.BakedInternalPrefabChildren), Variant.From(godotArray));
-    }
-
-    // To be deleted.
-    internal static bool IsNetworkObject(Node parent)
-    {
-        return HasScript(parent, nameof(NetworkObject));
-    }
-
-    // To be deleted.
-    internal static bool IsNetworkLevel(Node parent, string path)
-    {
-        return HasScript(parent, nameof(NetworkLevel));
-    }
-
-    // To be deleted.
-    internal static bool HasScript(Node parent, string scriptName)
-    {
-        foreach (var d in parent.GetPropertyList())
-        {
-            if (d["name"].AsString().Contains(scriptName))
-            {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    // To be deleted.
-    private static string GetLocalPathFromGlobal(string global)
-    {
-        var k = global.Replace(ProjectSettings.GlobalizePath("res://"), "").Insert(0, "res://");
-        k = k.Replace("\\", "/");
-        return k;
-    }
-
-    // To be deleted.
-    private List<string> GetAllFilesStartingWith(string path, string format, string startingWord)
-    {
-        var allfiles = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
-        var filtered = new List<string>(10);
-
-        foreach (var fileo in allfiles)
-        {
-            if (startingWord == null)
-            {
-                if (fileo.EndsWith(format))
-                    filtered.Add(fileo);
-            }
-            else
-            {
-                if (fileo.EndsWith(format) && fileo.ToLower().StartsWith(startingWord))
-                    filtered.Add(fileo);
-            }
-        }
-
-        return filtered;
-    }
-
-    // To be deleted.
-    private string[] LocalizeArrayElements(string[] array)
-    {
-        for (int i = 0; i < array.Length; i++)
-            array[i] = GetLocalPathFromGlobal(array[i]);
-        return array;
-    }
-
-    // To be deleted.
-    private void SaveNetickResource(Godot.Resource resource)
-    {
-        ResourceSaver.Save(resource, resource.ResourcePath);
     }
 }
 
